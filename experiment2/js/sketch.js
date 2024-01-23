f804875eb31d5b9279a34b9b2a327dd75d175efc
mrdoom <zqi22@ucsc.edu>
2024-01-22 19:47:39 -0800

    Update sketch.js

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,42 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let zoom = 10; 
+let maxZoom = 50; 
+let lastMouseX = 0; 
+let lastMouseY = 0;
+let colorValue = 0; 
+let rectWidth = zoom; 
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(1000, 1000);
+  rectMode(CENTER);
+  noStroke();
+  lastMouseX = mouseX; 
+  lastMouseY = mouseY; 
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(255);
+  if (mouseY < lastMouseY) {
+    zoom = min(zoom + 0.5, maxZoom);
+    colorValue = min(colorValue + 1, 255); 
+  } else if (mouseY > lastMouseY) {
+    zoom = max(zoom - 0.5, 10);
+    colorValue = max(colorValue - 1, 0); 
+  }
+  if (mouseX > lastMouseX) {
+    rectWidth = max(rectWidth - 1, 1);
+  } else if (mouseX < lastMouseX) {
+    rectWidth = min(rectWidth + 1, zoom);
+  }
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  for (let i = 0; i < width; i += zoom) {
+    for (let j = 0; j < height; j += zoom) {
+      let c = color(colorValue, 0, 255 - colorValue); 
+      fill(c);
+      let xOffset = random(-2, 2);
+      let yOffset = random(-2, 2);
+      rect(i + xOffset, j + yOffset, rectWidth, zoom);
+    }
+  }
+  lastMouseX = mouseX;
+  lastMouseY = mouseY;
 }
