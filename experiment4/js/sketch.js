0998e8afb0b98ea37627ce40239974e76dc8e53b
mrdoom <zqi22@ucsc.edu>
2024-02-06 20:15:40 -0800

    Updated experiment 4 and sky.png

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,135 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let bird;
+let gameState = 'start';
+let birdPath = [];
+let osc, env; 
+let currentPitchY = null; 
+let skyImage, birdImage;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+  skyImage = loadImage('image/sky.png');
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(1000, 400);
+  bird = new Bird();
+  textSize(32);
+  textAlign(CENTER, CENTER);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
-}
+  osc = new p5.Oscillator('sine');
+  osc.start();
+  osc.amp(0); 
 
-// draw() function is called repeatedly, it's the main animation loop
+  env = new p5.Envelope();
+  env.setADSR(0.1, 0.2, 0.5, 1); 
+  env.setRange(0.8, 0); 
+
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(skyImage);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  if (gameState === 'start') {
+    displayStartScreen();
+  } else if (gameState === 'playing') {
+    bird.update();
+    bird.show();
+  } else if (gameState === 'end') {
+    drawTrail();
+    displayPitchIndicator();
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function displayStartScreen() {
+  fill(255);
+  text("Press Space To Start", width / 2, height / 2);
+}
+
+function keyPressed() {
+  if (key === ' ' && gameState !== 'playing') {
+    gameState = 'playing';
+    bird.reset();
+    birdPath = [];
+  } else if (gameState === 'playing') {
+    bird.flap();
+  }
+}
+
+class Bird {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.y = height / 2;
+    this.x = 50;
+    this.velocity = 0;
+    this.gravity = 0.6;
+    this.lift = -15;
+  }
+
+  update() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    this.velocity *= 0.9; 
+
+    if (this.y > height) {
+      this.y = height;
+      gameState = 'end';
+      playSoundBasedOnPath();
+    }
+
+    if (this.x < width && gameState === 'playing') {
+      this.x += 3; 
+      birdPath.push({ x: this.x, y: this.y }); 
+    } else if (gameState === 'playing') {
+      gameState = 'end';
+      playSoundBasedOnPath();
+    }
+  }
+
+  flap() {
+    this.velocity += this.lift;
+  }
+
+  show() {
+    fill(255);
+    ellipse(this.x, this.y, 32, 32);
+  }
+}
+
+function drawTrail() {
+  noFill();
+  stroke(255, 100); 
+  beginShape();
+  birdPath.forEach(p => {
+    vertex(p.x, p.y);
+  });
+  endShape();
+}
+
+function playSoundBasedOnPath() {
+  birdPath.forEach((pos, index) => {
+    setTimeout(() => {
+      let freq = map(pos.y, 0, height, 100, 1000); 
+      osc.freq(freq);
+      env.triggerAttack(osc, 0); 
+      setTimeout(() => {
+        env.triggerRelease(osc); 
+      }, 100); 
+
+      currentPitchY = pos.y; 
+    }, index * 300); 
+  });
+
+  setTimeout(() => {
+    gameState = 'start';
+    birdPath = [];
+    currentPitchY = null; 
+  }, birdPath.length * 300 + 1000); 
+}
+
+
+function displayPitchIndicator() {
+  if (currentPitchY !== null) {
+    stroke(0, 0, 0); // Red line for the pitch indicator
+    line(0, currentPitchY, width, currentPitchY);
+  }
 }
