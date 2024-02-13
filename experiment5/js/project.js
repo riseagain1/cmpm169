def50d482282e29f45b81d363cd12de1cd4d2891
mrdoom <zqi22@ucsc.edu>
2024-02-12 17:07:21 -0800

    Updated experiment5 files

--- experiment5/js/project.js
+++ experiment5/js/project.js
@@ -1,54 +1,34 @@
-let checkerboard;
-let bgImage;
+// project.js - purpose and description here
+// Author: Your Name
+// Date:
 
-function preload() {
-  bgImage = loadImage('../img/board.png');
-}
+// NOTE: This is how we might start a basic JavaaScript OOP project
 
-function setup() {
-  createCanvas(1000, 1000, WEBGL);
-   generateCheckerboard(200, 10);
-}
+// Constants - User-servicable parts
+// In a longer project I like to put these in a separate file
 
-function draw() {
-  background(220);
+// define a class
+class MyProjectClass {
+  // constructor function
+  constructor(param1, param2) {
+    // set properties using 'this' keyword
+    this.property1 = param1;
+    this.property2 = param2;
+  }
   
-  ambientLight(60);
-  const lx = map(mouseX, 0, width, -1, 1);
-  const ly = map(mouseY, 0, height, -1, 1);
-  directionalLight(255, 255, 255, lx, ly, -0.5);
-  push(); 
-  translate(-width / 2, -height / 2, -500); 
-  imageMode(CORNER); 
-  image(bgImage, 0, 0, width, height);
-  pop(); 
-  push();
-  rotateY(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  texture(checkerboard);
-  box(100);
-  pop();
-  drawShadow(lx, ly);
-}
-
-
-function drawShadow(lx, ly) {
-  push();
-  noStroke();
-  fill(0, 75); 
-  translate(0, 150, 0); 
-  rotateX(HALF_PI);
-  ellipse(lx * 50, ly * 50, 200 - ly * 50, 100 - lx * 50);
-  pop();
-}
-
-function generateCheckerboard(size, squares) {
-  checkerboard = createGraphics(size, size); 
-  checkerboard.noStroke();
-  for (let i = 0; i < squares; i++) {
-    for (let j = 0; j < squares; j++) {
-      checkerboard.fill((i + j) % 2 == 0 ? 255 : 0);
-      checkerboard.rect(i * size / squares, j * size / squares, size / squares, size / squares);
-    }
+  // define a method
+  myMethod() {
+    // code to run when method is called
   }
 }
+
+function main() {
+  // create an instance of the class
+  let myInstance = new MyProjectClass(value1, value2);
+
+  // call a method on the instance
+  myInstance.myMethod();
+}
+
+// let's get this party started - uncomment me
+//main();
--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,56 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let checkerboard;
+let bgImage;
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+  bgImage = loadImage('../img/board.png');
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(1000, 1000, WEBGL);
+   generateCheckerboard(200, 10);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  background(220);
+  
+  ambientLight(60);
+  const lx = map(mouseX, 0, width, -1, 1);
+  const ly = map(mouseY, 0, height, -1, 1);
+  directionalLight(255, 255, 255, lx, ly, -0.5);
+  push(); 
+  translate(-width / 2, -height / 2, -500); 
+  imageMode(CORNER); 
+  image(bgImage, 0, 0, width, height);
+  pop(); 
+  push();
+  rotateY(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  texture(checkerboard);
+  box(100);
+  pop();
+  drawShadow(lx, ly);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+
+function drawShadow(lx, ly) {
+  push();
+  noStroke();
+  fill(0, 75); 
+  translate(0, 150, 0); 
+  rotateX(HALF_PI);
+  ellipse(lx * 50, ly * 50, 200 - ly * 50, 100 - lx * 50);
+  pop();
 }
+
+function generateCheckerboard(size, squares) {
+  checkerboard = createGraphics(size, size); 
+  checkerboard.noStroke();
+  for (let i = 0; i < squares; i++) {
+    for (let j = 0; j < squares; j++) {
+      checkerboard.fill((i + j) % 2 == 0 ? 255 : 0);
+      checkerboard.rect(i * size / squares, j * size / squares, size / squares, size / squares);
+    }
+  }
+}
+
+//Attribution: Code above is partially created by ChatGPT
